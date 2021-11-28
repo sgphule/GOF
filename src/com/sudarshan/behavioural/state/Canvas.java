c2e15ac6cac64c3a2679c5eeec1128d541090e5d
Sudarshan Phule <sgphule@gmail.com>
2021-11-28 21:35:39 +0100

    Build drawing application using state pattern

---
+++ src/com/sudarshan/behavioural/state/Canvas.java
@@ -0,0 +1,22 @@
+package com.sudarshan.behavioural.state;
+
+public class Canvas {
+    private Tool currentTool;
+
+    public void mouseDown(){
+        currentTool.mouseDown();
+    }
+
+    public void mouseUp(){
+        currentTool.mouseUp();
+    }
+
+    public Tool getCurrentTool() {
+        return currentTool;
+    }
+
+    public void setCurrentTool(Tool currentTool) {
+        this.currentTool = currentTool;
+    }
+
+}
---
+++ src/com/sudarshan/behavioural/state/Tool.java
@@ -0,0 +1,6 @@
+package com.sudarshan.behavioural.state;
+
+public interface Tool {
+    void mouseDown();
+    void mouseUp();
+}
