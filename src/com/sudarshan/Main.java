8cf6b530752eb87c3b77fda6fa8ad91b9cd7d299
Sudarshan Phule <sgphule@gmail.com>
2021-11-28 20:35:40 +0100

    Implement undo mechanism in editor using momento pattern

--- src/com/sudarshan/Main.java
+++ src/com/sudarshan/Main.java
@@ -1,8 +1,24 @@
 package com.sudarshan;
 
+import com.sudarshan.behavioural.momento.Editor;
+import com.sudarshan.behavioural.momento.History;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        var editor = new Editor();
+        var history = new History();
+
+        editor.setContent("first line");
+        history.push(editor.createState());
+
+        editor.setContent("second line");
+        history.push(editor.createState());
+
+        editor.setContent("third line");
+        editor.restore(history.pop());
+        editor.restore(history.pop());
+
+        System.out.println(editor.getContent());
     }
 }
